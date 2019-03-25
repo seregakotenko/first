abc9aba6010adfff7be654285e9b5336a10520d0
Serhii Kotenko <s.kotenko@astoundcommerce.com>
2019-03-25 14:48:51 +0200

    work defer load fonts

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,12 @@
 'use strict';
 
-var html = document.documentElement;
 var script = document.createElement("script");
 script.src = "./src/js/fontfaceobserver.js";
 script.async = true;
 
 script.onload = function () {
-    var regular = new FontFaceObserver("OpenSans");
-    var bold = new FontFaceObserver("OpenSans", {
+    var regular = new FontFaceObserver("Open Sans");
+    var bold = new FontFaceObserver("Open Sans", {
         weight: "bold"
     });
     
@@ -16,7 +15,7 @@ script.onload = function () {
         regular.load(),
         bold.load()
     ]).then(function () {
-        html.classList.add("fonts-loaded");
+        document.body.classList.add("fonts-loaded");
     });
 };
 document.head.appendChild(script);
