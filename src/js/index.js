772789dd963194baa46be2aa7f9efce156361674
Serhii Kotenko <s.kotenko@astoundcommerce.com>
2019-04-03 15:47:29 +0300

    work defer load fonts

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,66 @@
 'use strict';
 
-var script = document.createElement("script");
-script.src = "./src/js/fontfaceobserver.js";
-script.async = true;
+// var script = document.createElement("script");
+// script.src = "./src/js/fontfaceobserver.js";
+// script.async = true;
+//
+// script.onload = function () {
+//     var regular = new FontFaceObserver("Open Sans");
+//     var bold = new FontFaceObserver("Open Sans", {
+//         weight: "bold"
+//     });
+//
+//
+//     Promise.all([
+//         regular.load(),
+//         bold.load()
+//     ]).then(function () {
+//         document.body.classList.add("fonts-loaded");
+//     });
+// };
+// document.head.appendChild(script);
 
-script.onload = function () {
-    var regular = new FontFaceObserver("Open Sans");
-    var bold = new FontFaceObserver("Open Sans", {
-        weight: "bold"
+
+/******************/
+
+
+window.addEventListener('load', setLazy);
+window.addEventListener('load', lazyLoad);
+window.addEventListener('scroll', lazyLoad);
+
+var lazy = [];
+
+function setLazy() {
+    lazy = document.getElementsByClassName('lazy');
+    window.removeEventListener('load', setLazy);
+    console.log('Found ' + lazy.length + ' lazy images');
+}
+
+function lazyLoad() {
+    for (var i = 0; i < lazy.length; i++) {
+        if (isInViewport(lazy[i])) {
+            if (lazy[i].getAttribute('data-src')) {
+                lazy[i].src = lazy[i].getAttribute('data-src');
+                lazy[i].removeAttribute('data-src');
+            }
+        }
+    }
+    cleanLazy();
+}
+
+function cleanLazy() {
+    lazy = Array.prototype.filter.call(lazy, function (l) {
+        return l.getAttribute('data-src');
     });
-    
+}
 
-    Promise.all([
-        regular.load(),
-        bold.load()
-    ]).then(function () {
-        document.body.classList.add("fonts-loaded");
-    });
-};
-document.head.appendChild(script);
+function isInViewport(el) {
+    var rect = el.getBoundingClientRect();
 
+    return (
+      rect.bottom >= 0 &&
+      rect.right >= 0 &&
+      rect.top + 100 <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
